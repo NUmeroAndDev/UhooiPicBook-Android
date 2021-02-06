97e17ad703bda25fe0b3ec780d0bb09623bf604d
NUmeroAndDev <numero.dev.and@gmail.com>
2021-02-06 14:10:23 +0900

    Replaced fragment view with compose

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
@@ -8,12 +8,12 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.theuhooi.uhooipicbook.R
-import com.theuhooi.uhooipicbook.databinding.FragmentMonsterListBinding
 import com.theuhooi.uhooipicbook.modules.monsterlist.entities.MonsterItem
 import com.theuhooi.uhooipicbook.modules.monsterlist.viewmodels.MonsterListViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,19 +35,17 @@ class MonsterListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        setHasOptionsMenu(true)
-        val binding = FragmentMonsterListBinding.inflate(inflater, container, false)
-        binding.monsterListRecyclerview.adapter =
-            MonsterListRecyclerViewAdapter(
-                this.listener,
-                this.viewModel.monsters,
-                this.viewLifecycleOwner
-            )
-        binding.monsterListRecyclerview.layoutManager = LinearLayoutManager(this.context)
-        binding.viewModel = this.viewModel
-        binding.lifecycleOwner = this.viewLifecycleOwner
-        return binding.root
+    ): View = ComposeView(inflater.context).apply {
+        layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+
+        setContent {
+            MdcTheme {
+                MonsterListScreen(viewModel = viewModel)
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
