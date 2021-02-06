4766ed02d9072e2a0018b924a67af71da26c7548
NUmeroAndDev <numero.dev.and@gmail.com>
2021-02-06 13:47:46 +0900

    Moved composable

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
@@ -8,23 +8,6 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.preferredSize
-import androidx.compose.foundation.layout.preferredWidth
-import androidx.compose.material.AmbientTextStyle
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -34,7 +17,6 @@ import com.theuhooi.uhooipicbook.databinding.FragmentMonsterListBinding
 import com.theuhooi.uhooipicbook.modules.monsterlist.entities.MonsterItem
 import com.theuhooi.uhooipicbook.modules.monsterlist.viewmodels.MonsterListViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dev.chrisbanes.accompanist.coil.CoilImage
 
 @AndroidEntryPoint
 class MonsterListFragment : Fragment() {
@@ -109,48 +91,4 @@ class MonsterListFragment : Fragment() {
 
     // endregion
 
-}
-
-@Composable
-fun MonsterListItem(
-    modifier: Modifier = Modifier,
-    monsterItem: MonsterItem,
-) {
-    Card(
-        modifier = modifier.clickable {
-            // TODO navigate to detail
-        }
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CoilImage(
-                data = monsterItem.iconUrlString,
-                contentDescription = null,
-                modifier = Modifier.preferredSize(68.dp)
-            )
-            Spacer(modifier = Modifier.preferredWidth(16.dp))
-            Text(
-                text = monsterItem.name,
-                style = AmbientTextStyle.current.copy(
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            )
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewMonsterListItem() {
-    MonsterListItem(
-        monsterItem = MonsterItem(
-            name = "uhooi",
-            iconUrlString = "https://placehold.jp/150x150.png"
-        )
-    )
 }
